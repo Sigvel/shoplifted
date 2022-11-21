fee30622182c250807c9202c7bfad80f2556deac
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-11-21 14:39:03 +0100

    edits to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "/index.html",
-    "/pages/**/*.{html, js}",
-    "/src/js/**/*.{html, js}"
+    "./index.html",
+    "./pages/**/*.{html, js}",
+    "./src/js/**/*.{html, js}"
   ],
   theme: {
     extend: {},
