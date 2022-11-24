e357863501c525772554b2e1a4eca866eaf42610
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-11-24 10:24:31 +0100

    hamburger dropdown function

---
+++ src/js/components/hamburger.mjs
@@ -0,0 +1,15 @@
+const exitIcon = `<i class="fa-solid fa-xmark"></i>`
+const hamburgerIcon = `<i class="fa-solid fa-bars"></i>`
+
+export const hamburgerMenu = document.getElementById("mobile-menu");
+export const menuItems = document.getElementById("nav-items");
+
+export const menu = () => {
+
+    if( hamburgerMenu.innerHTML === exitIcon) {
+        hamburgerMenu.innerHTML = hamburgerIcon
+    } else {
+        hamburgerMenu.innerHTML = exitIcon
+    }
+    menuItems.classList.toggle("hidden");
+}
---
+++ src/js/index.mjs
@@ -0,0 +1,3 @@
+import { menu, hamburgerMenu } from "./components/hamburger.mjs"
+
+hamburgerMenu.addEventListener("click", menu);
