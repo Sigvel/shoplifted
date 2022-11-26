419909f246b1dedd1b51fef51e3ef1d966af0ae4
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-11-26 11:44:45 +0100

    Added fetch posts to index.mjs

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,3 +1,6 @@
+import { fetchPosts } from "./api/posts/read.mjs"
 import { menu, hamburgerMenu } from "./components/hamburger.mjs"
 
-hamburgerMenu.addEventListener("click", menu);
+hamburgerMenu.addEventListener("click", menu)
+
+fetchPosts()
