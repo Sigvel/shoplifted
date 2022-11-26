0a4da444b111ed85c02911df87a0de1a8586e863
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-11-26 12:10:01 +0100

    initial post html creation

---
+++ src/js/api/posts/components/post.mjs
@@ -0,0 +1,18 @@
+// const postHtml = (listings) => {
+
+// }
+
+{
+  /* <figure><img src="https://placeimg.com/400/225/arch" alt="Item" class="corner" /></figure>
+<div class="card-body bg-midnight-blue border border-paper-white mt-2">
+  <h2 class="card-title">
+    Item from api
+    <div class="badge badge-secondary">NEW</div>
+  </h2>
+  <p>If a dog chews shoes whose shoes does he choose?</p>
+  <div class="card-actions justify-end">
+    <div class="badge">Fashion</div> 
+    <div class="badge">Products</div>
+  </div>
+</div> */
+}
