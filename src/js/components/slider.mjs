1b246626f0e77a4e54b21d001c03efee68979312
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-11-28 13:47:05 +0100

    Slider js functionality

---
+++ src/js/components/slider.mjs
@@ -0,0 +1,40 @@
+export const slideRightBtn = document.getElementById("slide-right")
+export const slideLeftBtn = document.getElementById("slide-left")
+
+const card = document.getElementsByClassName("card-itm-group")
+
+let active = 0
+
+export const slideRight = () => {
+  const nextIndex = active + 1 <= card.length - 1 ? active + 1 : 0
+
+  const currentItem = document.querySelector(`[data-index="${active}"]`),
+    nextItem = document.querySelector(`[data-index="${nextIndex}"]`)
+
+  /**
+   * active becomes inactive
+   */
+  currentItem.dataset.status = "none"
+
+  setTimeout(() => {
+    nextItem.dataset.status = "active"
+    active = nextIndex
+  })
+}
+
+export const slideLeft = () => {
+  const nextIndex = active - 1 >= 0 ? active - 1 : card.length - 1
+
+  const currentItem = document.querySelector(`[data-index="${active}"]`),
+    nextItem = document.querySelector(`[data-index="${nextIndex}"]`)
+
+  /**
+   * active becomes inactive
+   */
+  currentItem.dataset.status = "none"
+
+  setTimeout(() => {
+    nextItem.dataset.status = "active"
+    active = nextIndex
+  })
+}
