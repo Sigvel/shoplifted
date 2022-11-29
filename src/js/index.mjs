856ad87abd64caf9bf0d8f230f5d48937821ca30
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-11-29 17:40:04 +0100

    slider listeners

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,6 +1,13 @@
 import { fetchPosts } from "./api/posts/read.mjs"
 import { menu, hamburgerMenu } from "./components/hamburger.mjs"
+import { slideLeftBtn, slideLeft, slideRightBtn, slideRight } from "./components/slider.mjs"
 
 hamburgerMenu.addEventListener("click", menu)
 
+// fetch posts
 fetchPosts()
+
+// slider button listeners
+slideRightBtn.addEventListener("click", slideRight)
+
+slideLeftBtn.addEventListener("click", slideLeft)
