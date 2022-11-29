5433748498d768d4fd35fcf73154b5f1b8cb6a65
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-11-29 19:15:20 +0100

    html listing elements creation

--- src/js/api/posts/components/post.mjs
+++ src/js/api/posts/components/post.mjs
@@ -1,18 +1,101 @@
-// const postHtml = (listings) => {
+/**
+ * creates the cards item container
+ * @returns
+ */
+export const cardContainer = () => {
+  const container = document.createElement("div")
+  container.className = "w-9/12 shadow-xl card-itm-group"
 
-// }
+  return container
+}
 
-{
-  /* <figure><img src="https://placeimg.com/400/225/arch" alt="Item" class="corner" /></figure>
-<div class="card-body bg-midnight-blue border border-paper-white mt-2">
-  <h2 class="card-title">
-    Item from api
-    <div class="badge badge-secondary">NEW</div>
-  </h2>
-  <p>If a dog chews shoes whose shoes does he choose?</p>
-  <div class="card-actions justify-end">
-    <div class="badge">Fashion</div> 
-    <div class="badge">Products</div>
-  </div>
-</div> */
+export const imageHtml = (media) => {
+  const figureBox = document.createElement("figure")
+  const img = document.createElement("img")
+  img.className = "corner"
+  img.src = media[0]
+
+  figureBox.appendChild(img)
+
+  return figureBox
+}
+
+const contentHeader = (title) => {
+  const contentTitle = document.createElement("h3")
+  contentTitle.className = "card-title text-sm"
+  contentTitle.textContent = title
+
+  return contentTitle
+}
+
+const description = (info) => {
+  const paragraph = document.createElement("p")
+  paragraph.className = "mt-1 text-sm"
+  paragraph.textContent = info
+
+  return paragraph
+}
+
+const tagsHtml = (tag) => {
+  const tagHtml = document.createElement("div")
+  tagHtml.className = "text-xs font-bold"
+  tagHtml.textContent = tag
+
+  return tagHtml
+}
+
+const bids = (bid) => {
+  let bidAmount = 0
+
+  for (let i = 0; i < bid.length; i++) {
+    bidAmount += bid[i].amount
+  }
+
+  // creates wrapper for the bids
+  const bidsWrapper = document.createElement("div")
+  bidsWrapper.className = "mt-10 flex justify-between"
+
+  // current bid object
+  const currentBid = document.createElement("p")
+  currentBid.className = "rounded bg-midnight-gray py-1 px-2 text-xs font-bold text-golden-brown"
+  currentBid.textContent = `Current bid: ${bidAmount}`
+
+  // latest bid object (is currently number of bids)
+  const bids = document.createElement("p")
+  bids.className = "rounded bg-midnight-gray py-1 px-2 text-xs font-bold text-golden-brown"
+  bids.textContent = `bids: ${bid.length}`
+
+  // Assembled big HTML node
+  bidsWrapper.append(currentBid, bids)
+
+  // returns the bid HTML node
+  return bidsWrapper
+}
+
+export const listingContent = (listing) => {
+  const tags = listing.tags
+
+  /**
+   * Container for tags
+   */
+  const tagsWrapper = document.createElement("div")
+  tagsWrapper.className = "card-actions mt-3 justify-start"
+
+  /**
+   * Creates tag html per tag.
+   */
+  tags.forEach((tag) => {
+    tagsWrapper.appendChild(tagsHtml(tag))
+  })
+
+  /**
+   * Html container for the content
+   */
+  const contentContainer = document.createElement("div")
+  contentContainer.className = "mt-2 border border-paper-white bg-midnight-blue p-2"
+
+  // appends content to the content container.
+  contentContainer.append(contentHeader(listing.title), description(listing.description), tagsWrapper, bids(listing.bids))
+
+  return contentContainer
 }
