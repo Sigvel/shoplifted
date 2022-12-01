84cb3d3b48aab9427f1bc19b0ad751791a6b6b83
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-12-01 13:58:45 +0100

    slider creation added

--- src/js/components/slider.mjs
+++ src/js/components/slider.mjs
@@ -1,70 +1,68 @@
-import * as create from "../api/posts/components/post.mjs"
+import * as create from "../api/posts/components/post.mjs";
 
-export const slideRightBtn = document.getElementById("slide-right")
-export const slideLeftBtn = document.getElementById("slide-left")
+export const slideRightBtn = document.getElementById("slide-right");
+export const slideLeftBtn = document.getElementById("slide-left");
 
-const card = document.getElementsByClassName("card-itm-group")
+const card = document.getElementsByClassName("card-itm-group");
 
-let active = 0
+let active = 0;
 
 export const slideRight = () => {
-  const nextIndex = active + 1 <= card.length - 1 ? active + 1 : 0
+  const nextIndex = active + 1 <= card.length - 1 ? active + 1 : 0;
 
   const currentItem = document.querySelector(`[data-index="${active}"]`),
-    nextItem = document.querySelector(`[data-index="${nextIndex}"]`)
+    nextItem = document.querySelector(`[data-index="${nextIndex}"]`);
 
   /**
    * active becomes inactive
    */
-  currentItem.dataset.status = "none"
+  currentItem.dataset.status = "none";
 
   setTimeout(() => {
-    nextItem.dataset.status = "active"
-    active = nextIndex
-  })
-}
+    nextItem.dataset.status = "active";
+    active = nextIndex;
+  });
+};
 
 export const slideLeft = () => {
-  const nextIndex = active - 1 >= 0 ? active - 1 : card.length - 1
+  const nextIndex = active - 1 >= 0 ? active - 1 : card.length - 1;
 
   const currentItem = document.querySelector(`[data-index="${active}"]`),
-    nextItem = document.querySelector(`[data-index="${nextIndex}"]`)
+    nextItem = document.querySelector(`[data-index="${nextIndex}"]`);
 
   /**
    * active becomes inactive
    */
-  currentItem.dataset.status = "none"
+  currentItem.dataset.status = "none";
 
   setTimeout(() => {
-    nextItem.dataset.status = "active"
-    active = nextIndex
-  })
-}
+    nextItem.dataset.status = "active";
+    active = nextIndex;
+  });
+};
 
-const cardSlider = document.getElementById("card-slider")
+const cardSlider = document.getElementById("card-slider");
 
 /**
  *
  * @param {*} response
  */
 export const createSliderPosts = (response) => {
-  console.log(response)
-
   response.map((listing, index) => {
     // Creating container the cardContainer
-    const container = create.cardContainer()
+    const container = create.cardContainer();
 
     if (index === 0) {
-      container.dataset.status = "active"
+      container.dataset.status = "active";
     } else {
-      container.dataset.status = "none"
+      container.dataset.status = "none";
     }
 
-    container.dataset.index = index
+    container.dataset.index = index;
 
-    container.append(create.imageHtml(listing.media), create.listingContent(listing))
+    container.append(create.imageHtml(listing.media), create.listingContent(listing));
 
     // appending to the section of the slider
-    cardSlider.appendChild(container)
-  })
-}
+    cardSlider.appendChild(container);
+  });
+};
