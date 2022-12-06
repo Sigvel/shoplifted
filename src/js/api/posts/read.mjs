e81089aa5ba8a437e4d102d5fe0708d4e54eeae2
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-12-06 19:10:34 +0100

    config to listing and slider html creation

--- src/js/api/posts/read.mjs
+++ src/js/api/posts/read.mjs
@@ -1,21 +1,39 @@
-import { apiUrl, listings } from "../constants.mjs"
-// import * as create from "./components/post.mjs";
-import { createSliderPosts } from "/src/js/components/slider.mjs"
+import { apiUrl, listings } from "../constants.mjs";
+import * as create from "./components/listing.mjs";
+import { createSliderPosts } from "/src/js/components/slider.mjs";
 
-// export let listingsArray = []
+/**
+ * Listings container
+ */
+const listingsContainer = document.getElementById("listings");
+
+export let listingsArray = [];
 
 export async function fetchPosts() {
   try {
-    const response = await fetch(`${apiUrl}${listings}?_seller=true&_bids=true`)
-    const json = await response.json()
+    const response = await fetch(`${apiUrl}${listings}?_seller=true&_bids=true`);
+    const json = await response.json();
 
     if (response.ok) {
-      // listingsArray = json
-      createSliderPosts(json)
+      listingsArray = json;
+      if (location.pathname === "/index.html") {
+        createSliderPosts(json);
+      }
+      createListingPosts();
     } else {
-      throw new Error(response.statusText)
+      throw new Error(response.statusText);
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
+
+const createListingPosts = () => {
+  listingsArray.map((listings) => {
+    const card = create.card();
+
+    card.append(create.listingContent(listings));
+
+    listingsContainer.appendChild(card);
+  });
+};
