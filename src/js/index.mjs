9167f326ca1420785d89942fed946daad4d31b34
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-12-06 19:10:07 +0100

    items targeting index path

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,13 +1,15 @@
-import { fetchPosts } from "./api/posts/read.mjs"
-import { menu, hamburgerMenu } from "./components/hamburger.mjs"
-import { slideLeftBtn, slideLeft, slideRightBtn, slideRight } from "./components/slider.mjs"
+import { fetchPosts } from "./api/posts/read.mjs";
+import { menu, hamburgerMenu } from "./components/hamburger.mjs";
+import { slideLeftBtn, slideLeft, slideRightBtn, slideRight } from "./components/slider.mjs";
 
-hamburgerMenu.addEventListener("click", menu)
+hamburgerMenu.addEventListener("click", menu);
 
 // fetch posts
-fetchPosts()
+fetchPosts();
 
-// slider button listeners
-slideRightBtn.addEventListener("click", slideRight)
+if (location.pathname === "/index.html") {
+  // slider button listeners
+  slideRightBtn.addEventListener("click", slideRight);
 
-slideLeftBtn.addEventListener("click", slideLeft)
+  slideLeftBtn.addEventListener("click", slideLeft);
+}
