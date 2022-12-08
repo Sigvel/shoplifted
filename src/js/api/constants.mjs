53083b9250bdb805e2399b08a696ea4e0c7b4420
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-12-08 07:07:25 +0100

    profile urls

--- src/js/api/constants.mjs
+++ src/js/api/constants.mjs
@@ -1,2 +1,4 @@
-export const apiUrl = "https://nf-api.onrender.com/api/v1"
-export const listings = "/auction/listings"
+export const apiUrl = "https://nf-api.onrender.com/api/v1";
+export const listings = "/auction/listings";
+export const profileUrl = "/auction/profiles/<name>?_listings=true";
+export const bids = "/auction/profiles/<name>/bids?_listings=true";
