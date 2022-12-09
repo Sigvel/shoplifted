13d9c98490dc92ed58915a2e64a82e87c49c09b9
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-12-09 06:21:43 +0100

    new url paths

--- src/js/api/constants.mjs
+++ src/js/api/constants.mjs
@@ -2,3 +2,5 @@ export const apiUrl = "https://nf-api.onrender.com/api/v1";
 export const listings = "/auction/listings";
 export const profileUrl = "/auction/profiles/<name>?_listings=true";
 export const bids = "/auction/profiles/<name>/bids?_listings=true";
+export const REG_URL = "/auction/auth/register";
+export const LOGIN_URL = "/auction/auth/login";
