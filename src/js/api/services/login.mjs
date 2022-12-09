5e77b4a631399126f7aba37b0a84ab3a58d8c647
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-12-09 08:55:16 +0100

    login functionality

---
+++ src/js/api/services/login.mjs
@@ -0,0 +1,36 @@
+import { apiUrl, LOGIN_URL } from "../constants.mjs";
+import { errorMessage } from "../../components/errorMsg.mjs";
+import { loginForm } from "./handlers/login.mjs";
+import { save } from "../storage/save.mjs";
+
+export async function loginUser(userData) {
+  const body = JSON.stringify(userData);
+
+  try {
+    const response = await fetch(`${apiUrl}${LOGIN_URL}`, {
+      method: "post",
+      body,
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+    });
+
+    const { accessToken, ...user } = await response.json();
+
+    if (response.ok) {
+      save("token", accessToken);
+      save("user", user);
+      errorMessage("Login successful", true);
+    }
+
+    errorMessage(user.errors[0].message, false);
+
+    throw new Error(await response.statusText);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    setTimeout(() => {
+      loginForm.reset();
+    }, 1000);
+  }
+}
