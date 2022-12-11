c19bfb96afb1fcc93feb1062d4f5f32c6507cba9
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-12-11 04:59:41 +0100

    error message function

--- src/js/components/errorMsg.mjs
+++ src/js/components/errorMsg.mjs
@@ -1,4 +1,4 @@
-export const feedback = document.getElementById("feedback-msg");
+export const feedback = document.querySelectorAll(".feedback-msg");
 
 export function errorMessage(message, status) {
   let bgColor = "bg-error";
@@ -11,7 +11,9 @@ export function errorMessage(message, status) {
     color = "text-success-txt";
   }
 
-  feedback.innerHTML = `<div class="${bgColor} w-full py-1">
-  <p class="${color} text-center font-bold">${message}</p>
-  </div>`;
+  feedback.forEach((container) => {
+    container.innerHTML = `<div class="${bgColor} w-full px-1">
+    <p class="${color} text-center font-bold">${message}</p>
+    </div>`;
+  });
 }
