fc39a59d78ae102b426651de2dc2bb7cfb04922f
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-12-16 12:37:37 +0100

    created fetch specific listing function

--- src/js/api/posts/read.mjs
+++ src/js/api/posts/read.mjs
@@ -1,6 +1,7 @@
 import { apiUrl, listings } from "../constants.mjs";
-import * as create from "./templates/listing.mjs";
+import * as create from "./templates/listings.mjs";
 import { createSliderPosts } from "/src/js/components/slider.mjs";
+import { listingBids } from "./templates/bid.mjs";
 
 /**
  * Listings container
@@ -9,7 +10,7 @@ const listingsContainer = document.getElementById("listings");
 
 export let listingsArray = [];
 
-export async function fetchPosts() {
+export async function fetchListings() {
   try {
     const response = await fetch(`${apiUrl}${listings}?_seller=true&_bids=true`);
     const json = await response.json();
@@ -18,8 +19,9 @@ export async function fetchPosts() {
       listingsArray = json;
       if (location.pathname === "/index.html") {
         createSliderPosts(json);
+        console.log(json);
       } else if (location.pathname === "/pages/listing/index.html") {
-        createListingPosts();
+        createListings();
       }
     } else {
       throw new Error(response.statusText);
@@ -29,12 +31,54 @@ export async function fetchPosts() {
   }
 }
 
-const createListingPosts = () => {
+const createListings = () => {
   listingsArray.map((listings) => {
     const card = create.card();
 
-    card.append(create.listingContent(listings));
+    card.append(create.listingsContent(listings));
 
     listingsContainer.appendChild(card);
   });
+};
+
+export async function fetchListing(url) {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+
+    if (response.ok) {
+      createListing(json);
+    } else {
+      throw new Error(response.statusText);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// listing container.
+const listingSection = document.getElementById("listing");
+
+// Other html elements
+const seller = document.getElementById("seller");
+const sellerImg = document.getElementById("seller-img");
+
+const bidsSection = document.getElementById("listing-bids");
+
+// const listingBids = document.getElementById("listing-bids");
+
+const createListing = (listing) => {
+  const bids = listing.bids.sort((a, b) => {
+    if (a.created.toLowerCase() < b.created.toLowerCase()) return 1;
+    return -1;
+  });
+
+  bids.forEach((bid) => {
+    bidsSection.append(listingBids(bid));
+  });
+
+  seller.innerHTML = listing.seller.name;
+  sellerImg.src = listing.seller.avatar;
+
+  listingSection.appendChild(create.listing(listing));
 };
