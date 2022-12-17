619fe6acf79c1ca93286e5f06be3c368758b098c
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-12-17 08:11:14 +0100

    added profile alternative

--- src/js/api/constants.mjs
+++ src/js/api/constants.mjs
@@ -11,5 +11,6 @@ export const listings = "/auction/listings";
 export const profileUrl = `/auction/profiles/${userName}?_listings=true`;
 export const MEDIA_update = `/auction/profiles/${userName}/media`;
 export const bids = `/auction/profiles/${userName}/bids?_listings=true`;
+export const profileAlt = `/auction/profiles/${userName}/listings?bids=true`;
 export const REG_URL = "/auction/auth/register";
 export const LOGIN_URL = "/auction/auth/login";
