248148dd80270d4fcc7ae449cbfe3afbb9064f9b
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-12-18 14:18:40 +0100

    removed add new button to new input element

--- src/js/api/posts/templates/inputUrl.mjs
+++ src/js/api/posts/templates/inputUrl.mjs
@@ -3,24 +3,12 @@ export const createUrl = () => {
   container.className = "flex w-11/12 lg:w-8/12 justify-between mb-3";
 
   const urlInput = document.createElement("input");
-  urlInput.className = "py-1 px-1 rounded w-11/12 lg:w-8/12 md:text-base";
+  urlInput.className = "py-1 px-1 w-full rounded md:text-base";
   urlInput.type = "text";
   urlInput.dataset.type = "url";
   urlInput.placeholder = "media url";
 
-  const addButton = document.createElement("button");
-  addButton.className = "text-paper-white rounded bg-sky-blue py-1 px-2 font-bold";
-  addButton.dataset.type = "add-btn";
-  addButton.textContent = "+";
-
-  container.append(urlInput, addButton);
+  container.append(urlInput);
 
   return container;
 };
-
-{
-  /* <div class="flex w-11/12 lg:w-8/12 justify-between mb-3 hidden">
-<input data-type="url" name="media" type="text" class="py-1 px-1 rounded w-11/12 lg:w-8/12 md:text-base" placeholder="media url" disabled>
-<button data-type="add-btn" type="button" class="url-add_btn text-paper-white rounded bg-sky-blue py-1 px-2 font-bold">+</button>
-</div> */
-}
