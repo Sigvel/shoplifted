bb34472903450ecb57e1155814827aad59961c87
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-12-18 20:28:46 +0100

    added page reload to logout

--- src/js/api/services/logout.mjs
+++ src/js/api/services/logout.mjs
@@ -3,14 +3,17 @@ import { load } from "../storage/load.mjs";
 const loginButton = document.querySelector(".loginBtn");
 const profileLink = document.querySelector(".profile-link");
 const profileLinkLabel = document.querySelector(".profile-label");
+const loginLabel = document.querySelector(".login-label");
 
 export const isLoggedIn = () => {
   if (load("token") === null) {
     loginButton.innerHTML = "login";
     profileLink.removeAttribute("href");
     profileLinkLabel.setAttribute("for", "login-modal");
+    loginLabel.setAttribute("for", "login-modal");
     return;
   }
+  loginLabel.removeAttribute("for", "login-modal");
   loginButton.innerHTML = "logout";
   loginButton.addEventListener("click", logout);
   return;
@@ -18,4 +21,8 @@ export const isLoggedIn = () => {
 
 export const logout = () => {
   localStorage.clear();
+
+  setTimeout(() => {
+    location.reload();
+  }, 1500);
 };
