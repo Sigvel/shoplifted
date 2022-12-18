5f83cf191216a25350eec09a93b106013489d5af
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-12-18 18:06:29 +0100

    edited style for error message return

--- src/js/components/errorMsg.mjs
+++ src/js/components/errorMsg.mjs
@@ -11,7 +11,7 @@ export function errorMessage(message, status) {
     color = "text-success-txt";
   }
 
-  return `<div class="${bgColor} w-full px-2">
+  return `<div class="${bgColor} w-11/12 lg:w-8/12 mx-auto px-2 py-1 my-2">
     <p class="${color} text-center font-bold">${message}</p>
     </div>`;
 }
