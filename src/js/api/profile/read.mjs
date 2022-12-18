25c37f02150d21274ffa9d2a898e3f6c9cb832ba
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-12-18 14:20:17 +0100

    rearanged javascript to include credits to all pages

--- src/js/api/profile/read.mjs
+++ src/js/api/profile/read.mjs
@@ -2,37 +2,47 @@ import { apiUrl, profileUrl } from "../constants.mjs";
 import { options } from "../auth/header.mjs";
 
 // HTML targets
-const holdings = document.querySelectorAll(".curr-holdings");
-const listings = document.getElementById("listings");
+const usersListings = document.getElementById("listings");
 const bidWins = document.getElementById("bids-won");
 const userName = document.getElementById("username");
-const media = document.getElementById("profileImg");
+const profileMedia = document.getElementById("profileImg");
 
 export async function fetchProfile() {
   try {
     const response = await fetch(`${apiUrl}${profileUrl}`, options);
     const json = await response.json();
 
-    buildProfile(json);
+    if (location.pathname === "/pages/profile/index.html") {
+      buildProfile(json);
+      checkCredits(json.credits);
+    } else {
+      checkCredits(json.credits);
+    }
   } catch (error) {
     console.log(error);
   }
 }
 
+export let userCredits;
+export const holdings = document.querySelectorAll(".curr-holdings");
+
+export const checkCredits = (userCredits) => {
+  holdings.forEach((container) => {
+    container.innerHTML = `${userCredits}`;
+  });
+};
+
 const buildProfile = (profile) => {
   /** setting current currency to nav and profile */
-  holdings.forEach((container) => {
-    container.innerHTML = `${profile.credits}`;
-  });
-  if (location.href === "/pages/profile/index.html") {
-    if (profile.avatar === null) {
-      media.src = "/assets/images/placeholder/profile/avatar-g70ad01aee_640.png";
-    } else {
-      media.src = profile.avatar;
-    }
+  console.log(profile);
 
-    listings.innerHTML = `${profile.listings.length}`;
-    bidWins.innerHTML = `${profile.wins.length}`;
-    userName.innerHTML = `${profile.name}`;
+  if (!profile.avatar) {
+    profileMedia.src = "/assets/images/placeholder/profile/avatar-g70ad01aee_640.png";
+  } else {
+    profileMedia.src = profile.avatar;
   }
+
+  usersListings.innerHTML = `${profile.listings.length}`;
+  bidWins.innerHTML = `${profile.wins.length}`;
+  userName.innerHTML = `${profile.name}`;
 };
